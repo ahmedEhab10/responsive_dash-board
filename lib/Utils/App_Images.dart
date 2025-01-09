796832e661de793c7ed3dd6dabab2daf8b985295
ahmedEhab10 <ahmedhobaa24@gmail.com>
2025-01-09 23:43:44 +0200

    Add app images

---
+++ lib/Utils/App_Images.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBalanced
+  /// assets/images/Balanced.svg
+  static const String imagesBalanced = "assets/images/Balanced.svg";
+
+  /// Assets for imagesCardBackground
+  /// assets/images/card_background.svg
+  static const String imagesCardBackground = "assets/images/card_background.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/Expenses.svg
+  static const String imagesExpenses = "assets/images/Expenses.svg";
+
+  /// Assets for imagesFace1
+  /// assets/images/face1.svg
+  static const String imagesFace1 = "assets/images/face1.svg";
+
+  /// Assets for imagesFace2
+  /// assets/images/face2.svg
+  static const String imagesFace2 = "assets/images/face2.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMyInvestments
+  /// assets/images/my_investments.svg
+  static const String imagesMyInvestments = "assets/images/my_investments.svg";
+
+  /// Assets for imagesMyTransaction
+  /// assets/images/my_transaction.svg
+  static const String imagesMyTransaction = "assets/images/my_transaction.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/Statistics.svg
+  static const String imagesStatistics = "assets/images/Statistics.svg";
+
+  /// Assets for imagesWallerAccount
+  /// assets/images/waller_account.svg
+  static const String imagesWallerAccount = "assets/images/waller_account.svg";
+}
+
