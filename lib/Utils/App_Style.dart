eb73d109b0b1bb10cd5873fdb458fc582d1ae408
ahmedEhab10 <ahmedhobaa24@gmail.com>
2025-01-09 23:18:51 +0200

    make app style

---
+++ lib/Utils/App_Style.dart
@@ -0,0 +1,97 @@
+import 'dart:ui';
+
+import 'package:responsive_dash_board/utils/size_config.dart';
+
+abstract class AppStyles {
+  static TextStyle styleRegular16 = TextStyle(
+    color: const Color(0xFF064060),
+    fontSize: getResponsiveFontSize(fontSize: 16),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle styleBold16 = TextStyle(
+    color: const Color(0xFF4EB7F2),
+    fontSize: getResponsiveFontSize(fontSize: 16),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle styleMedium16 = TextStyle(
+    color: const Color(0xFF064061),
+    fontSize: getResponsiveFontSize(fontSize: 16),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle styleMedium20 = TextStyle(
+    color: const Color(0xFFFFFFFF),
+    fontSize: getResponsiveFontSize(fontSize: 20),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle styleSemiBold16 = TextStyle(
+    color: const Color(0xFF064061),
+    fontSize: getResponsiveFontSize(fontSize: 16),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle styleSemiBold20 = TextStyle(
+    color: const Color(0xFF064061),
+    fontSize: getResponsiveFontSize(fontSize: 20),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleRegular12 = TextStyle(
+    color: const Color(0xFFAAAAAA),
+    fontSize: getResponsiveFontSize(fontSize: 12),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle styleSemiBold24 = TextStyle(
+    color: const Color(0xFF4EB7F2),
+    fontSize: getResponsiveFontSize(fontSize: 24),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle styleRegular14 = TextStyle(
+    color: const Color(0xFFAAAAAA),
+    fontSize: getResponsiveFontSize(fontSize: 14),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle styleSemiBold18 = TextStyle(
+    color: const Color(0xFFFFFFFF),
+    fontSize: getResponsiveFontSize(fontSize: 18),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+}
+
+// sacleFactor
+// responsive font size
+// (min , max) fontsize
+double getResponsiveFontSize({required double fontSize}) {
+  double scaleFactor = getScaleFactor();
+  double responsiveFontSize = fontSize * scaleFactor;
+
+  double lowerLimit = fontSize * .8;
+  double upperLimit = fontSize * 1.2;
+
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor() {
+  var dispatcher = PlatformDispatcher.instance;
+  var physicalWidth = dispatcher.views.first.physicalSize.width;
+  var devicePixelRatio = dispatcher.views.first.devicePixelRatio;
+  double width = physicalWidth / devicePixelRatio;
+  if (width < SizeConfig.tablet) {
+    return width / 550;
+  } else if (width < SizeConfig.desktop) {
+    return width / 1000;
+  } else {
+    return width / 1920;
+  }
+}
--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/adaptive_layout.dart';
+import 'package:responsive_dash_board/views/widgets/desktopview.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return AdaptiveLayout(
+      mobileLayout: (BuildContext context) => Text('mobile Layout'),
+      tabletLayout: (BuildContext context) => Text('Tablet Layout'),
+      desktopLayout: (BuildContext context) => Desktopview(),
+    );
   }
 }
